b400c13be0401e21457c650bd108864812d54551
ypablo <yfedora@gmail.com>
2019-10-01 23:17:56 +0100

    Add router dom

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { FaHome } from 'react-icons/fa';
-import styled from 'styled-components';
+import { Route, Switch } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      Hello from App.js <FaHome />
-      <Button>Hello styled.components</Button>
-    </div>
-  );
+import Home from './pages/HomePage'
+import About from './pages/AboutPage'
+import Products from './pages/Products'
+import Contact from './pages/ContactPage'
+import SingleProduct from './pages/SingleProductPage'
+import Default from './pages/Default'
+import Cart from './pages/CartPage'
+
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Sidebar from './components/Sidebar'
+import SideCart from './components/SideCart'
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {/*navbar, sidebar, cart, footer*/}
+        <Navbar />
+        <Sidebar />
+        <SideCart />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/products" exact component={Products} />
+          <Route path="/products/:id" component={SingleProduct} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
+        <Footer />
+      </div>
+    )
+  }
 }
 
-const Button = styled.button`
-  color: white;
-  background:#f15025;
-  font-size: 2rem;
-`
 
 export default App;
