50ada1404a43d8ebdb890b8cd7b83464be0e5671
ypablo <yfedora@gmail.com>
2019-10-02 21:03:01 +0100

    Add nav-bar

--- src/context/context.js
+++ src/context/context.js
@@ -3,14 +3,41 @@ import React, { Component } from 'react'
 const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
+    state = {
+        sidebarOpen: false,
+        cartOpen: false,
+        cartItems: 0
+    }
+    //handle side bar
+    handleSidebar = () => {
+        this.setState({ sidebarOpen: !this.state.sidebarOpen })
+    }
+    //handle cart
+    handleCart = () => {
+        this.setState({ cartOpen: !this.state.cartOpen })
+    }
 
+    //open cart
+    closeCart = () => {
+        this.setState({ cartOpen: false })
+    }
 
+    //close cart
+    openCart = () => {
+        this.setState({ cartOpen: true })
+    }
 
     render() {
         return (
-            <ProductContext.Provider value="hello from context">
+            <ProductContext.Provider value={{
+                ...this.state,
+                handleSidebar: this.handleSidebar,
+                handleCart: this.handleCart,
+                closeCart: this.closeCart,
+                openCart: this.openCart
+            }}>
                 {this.props.children}
-            </ProductContext.Provider>
+            </ProductContext.Provider >
         )
     }
 }
