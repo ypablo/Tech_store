fc909840d40dea5db4b70846f9cc6e375b5b0ce8
ypablo <yfedora@gmail.com>
2019-10-05 10:34:31 +0100

    Main page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { ProductConsumer } from '../context/context'
+import Hero from '../components/Hero'
+import {Link} from 'react-router-dom'
+import App from '../App';
 
 export default function HomePage() {
     return (
         <div>
-            <ProductConsumer>
-                {value => {
-                    console.log(value)
-                    return <h1>Hello form home page</h1>
-                }}
-            </ProductConsumer>
+            <Hero title="awesome gadgets" max="true">
+                <Link to="/products" className="main-link" style={{margin:"2rem"}}>
+                    our products
+                </Link>
+            </Hero>
         </div>
     )
 }
