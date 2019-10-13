e313be912ffbac7c22fc86b7a34c9afeafa1391e
ypablo <yfedora@gmail.com>
2019-10-13 12:21:48 +0100

    Add cart logic

--- src/components/CartPage/CartItem.js
+++ src/components/CartPage/CartItem.js
@@ -1,44 +1,62 @@
-import React from 'react'
-import { FaTrash, FaChevronCircleUp, FaChevronCircleDown } from 'react-icons/fa'
+import React from "react";
+import {
+  FaTrash,
+  FaChevronCircleUp,
+  FaChevronCircleDown
+} from "react-icons/fa";
 
+export default function CartItem({cartItem, increment, decrement, removeItem}) {
+  const { id, title, price, count, total, image } = cartItem;
 
-export default function CartItem({ CartItem, increment, decrement, removeItem }) {
-    const { id, title, price, count, total, image } = CartItem
-    return (
-        <div className="row mt-5 mt-lg-0 text-capitalize text-center align-items-center">
-            {/*image*/}
-            <div className="col-10 mx-auto col-lg-2 pb-2">
-                <img src={image} alt="" width="60" className="img-fluid" />
-            </div>
-            {/*title*/}
-            <div className="col-10 mx-auto col-lg-2 pb-2">
-                <span className="d-lg-none">product : </span>
-                {title}
-            </div>
-            {/*price*/}
-            <div className="col-10 mx-auto col-lg-2 pb-2">
-                <span className="d-lg-none">price : $</span>
-                {price}
-            </div>
-            {/*count controls*/}
-            <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
-                <div className="d-flex justify-content-center">
-                    <div>
-                        <FaChevronCircleDown className="text-primary cart-icon" onClick={() => decrement(id)} />
-                        <span className="text-title text-muted mx-3">{count}</span>
-                        <FaChevronCircleUp className="text-primary cart-icon" onClick={() => increment(id)} />
-                    </div>
-                </div>
-            </div>
-            {/*remove item*/}
-            <div className="col-10 mx-auto col-lg-2">
-                <FaTrash className="text-danger cart-icon" onClick={() => removeItem(id)} />
-            </div>
-            {/*item total*/}
-            <div className="col-10 mx-auto col-lg-2">
-                <strong className="text-muted">item total : ${total}</strong>
-
-            </div>
+  return (
+    <div className="row mt-5 mt-lg-0 text-capitalize text-center align-items-center">
+      {/* image */}
+      <div className="col-10 mx-auto col-lg-2 pb-2">
+        <img src={image} width="60" className="img-fluid" alt="product" />
+      </div>
+      {/*end of image */}
+      {/* title */}
+      <div className="col-10 mx-auto col-lg-2 pb-2">
+        <span className="d-lg-none">product : </span>
+        {title}
+      </div>
+      {/*end of title */}
+      {/* price */}
+      <div className="col-10 mx-auto col-lg-2 pb-2">
+        <span className="d-lg-none">price : $</span>
+        {price}
+      </div>
+      {/*end of price */}
+      {/* count controls */}
+      <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
+        <div className="d-flex justify-content-center">
+          <div>
+            <FaChevronCircleDown
+              onClick={() => decrement(id)}
+              className="cart-icon text-primary"
+            />
+            <span className="text-title text-muted mx-3">{count}</span>
+            <FaChevronCircleUp
+              onClick={() => increment(id)}
+              className="cart-icon text-primary"
+            />
+          </div>
         </div>
-    )
+      </div>
+      {/* end count controls */}
+      {/* remove item */}
+      <div className="col-10 mx-auto col-lg-2 ">
+        <FaTrash
+          className="text-danger cart-icon"
+          onClick={() => removeItem(id)}
+        />
+      </div>
+      {/*end of remove */}
+      {/* item total */}
+      <div className="col-10 mx-auto col-lg-2">
+        <strong className="text-muted">item total : ${total}</strong>
+      </div>
+      {/* end item total */}
+    </div>
+  );
 }
--- src/context/context.js
+++ src/context/context.js
@@ -1,193 +1,253 @@
-import React, { Component } from 'react'
-import { linkData } from './linkData'
-import { socialData } from './socialData'
-import { items } from './productData'
-
+import React, { Component } from "react";
+import { linkData } from "./linkData";
+import { socialData } from "./socialData";
+import { items } from "./productData";
 const ProductContext = React.createContext();
-
+//Provider
+//Consumer
 class ProductProvider extends Component {
-    state = {
-        sidebarOpen: false,
-        cartOpen: false,
-        cartItems: 0,
-        links: linkData,
-        socialIcons: socialData,
-        cart: [],
-        cartSubTotal: 0,
-        cartTax: 0,
-        cartTotal: 0,
-        storeProducts: [],
-        filteredProducts: [],
-        featuredProducts: [],
-        singleProduct: {},
-        loading: true
-    }
+  state = {
+    sidebarOpen: false,
+    cartOpen: false,
+    links: linkData,
+    socialIcons: socialData,
+    cart: [],
+    cartItems: 0,
+    cartSubTotal: 0,
+    cartTax: 0,
+    carTotal: 0,
+    storeProducts: [],
+    filteredProducts: [],
+    featuredProducts: [],
+    singleProduct: {},
+    loading: true
+  };
+  componentDidMount() {
+    //from contentful items
 
-    componentDidMount() {
-        //from contentful items
-        this.setProducts(items);
-    }
+    this.setProducts(items);
+  }
 
-    //set Products
-    setProducts = (products) => {
-        let storeProducts = products.map(item => {
-            const { id } = item.sys
-            const image = item.fields.image.fields.file.url
-            const product = { id, ...item.fields, image }
-            return product
-        })
-        //featured products
-        let featuredProducts = storeProducts.filter(item => item.featured === true);
-        this.setState({
-            storeProducts,
-            filteredProducts: storeProducts,
-            featuredProducts,
-            cart: this.getStorageCart(),
-            singleProduct: this.getStorageProduct(),
-            loading: false
-        }, () => { this.addTotals() })
+  //set products
+
+  setProducts = products => {
+    let storeProducts = products.map(item => {
+      const { id } = item.sys;
+      const image = item.fields.image.fields.file.url;
+      const product = { id, ...item.fields, image };
+      return product;
+    });
+    //  featured products
+    let featuredProducts = storeProducts.filter(item => item.featured === true);
+    this.setState(
+      {
+        storeProducts,
+        filteredProducts: storeProducts,
+        featuredProducts,
+        cart: this.getStorageCart(),
+        singleProduct: this.getStorageProduct(),
+        loading: false
+      },
+      () => {
+        this.addTotals();
+      }
+    );
+  };
+  // get cart from local storage
+  getStorageCart = () => {
+    let cart;
+    if (localStorage.getItem("cart")) {
+      cart = JSON.parse(localStorage.getItem("cart"));
+    } else {
+      cart = [];
     }
-    //get cart from local storage
-    getStorageCart = () => {
-        let cart
-        if (localStorage.getItem("cart")) {
-            cart = JSON.parse(localStorage.getItem("cart"))
-        } else {
-            cart = []
-        }
-        return cart
+    return cart;
+  };
+  // get product from local storage
+  getStorageProduct = () => {
+    return localStorage.getItem("singleProduct")
+      ? JSON.parse(localStorage.getItem("singleProduct"))
+      : {};
+  };
+  // get totals
+  getTotals = () => {
+    let subTotal = 0;
+    let cartItems = 0;
+    this.state.cart.forEach(item => {
+      subTotal += item.total;
+      cartItems += item.count;
+    });
+
+    subTotal = parseFloat(subTotal.toFixed(2));
+    let tax = subTotal * 0.2;
+    tax = parseFloat(tax.toFixed(2));
+    let total = subTotal + tax;
+    total = parseFloat(total.toFixed(2));
+    return {
+      cartItems,
+      subTotal,
+      tax,
+      total
     };
+  };
+  //add totals
+  addTotals = () => {
+    const totals = this.getTotals();
+    this.setState({
+      cartItems: totals.cartItems,
+      cartSubTotal: totals.subTotal,
+      cartTax: totals.tax,
+      cartTotal: totals.total
+    });
+  };
+  // sync storage
+  syncStorage = () => {
+    localStorage.setItem("cart", JSON.stringify(this.state.cart));
+  };
+  //add to cart
+  addToCart = id => {
+    let tempCart = [...this.state.cart];
+    let tempProducts = [...this.state.storeProducts];
+    let tempItem = tempCart.find(item => item.id === id);
+    if (!tempItem) {
+      tempItem = tempProducts.find(item => item.id === id);
+      let total = tempItem.price;
+      let cartItem = { ...tempItem, count: 1, total };
+      tempCart = [...tempCart, cartItem];
+    } else {
+      tempItem.count++;
+      tempItem.total = tempItem.price * tempItem.count;
+      tempItem.total = parseFloat(tempItem.total.toFixed(2));
+    }
+    this.setState(
+      () => {
+        return { cart: tempCart };
+      },
+      () => {
+        this.addTotals();
+        this.syncStorage();
+        this.openCart();
+      }
+    );
+  };
+  // set single product
+  setSingleProduct = id => {
+    let product = this.state.storeProducts.find(item => item.id === id);
+    localStorage.setItem("singleProduct", JSON.stringify(product));
+    this.setState({
+      singleProduct: { ...product },
+      loading: false
+    });
+  };
 
-    getStorageProduct = () => {
-        return localStorage.getItem("singleProduct") ? JSON.parse(localStorage.getItem("singleProduct")) : {}
-    };
-
-    //get totals
-    getTotals = () => {
-        let subTotal = 0
-        let cartItems = 0
-        this.state.cart.forEach(item => {
-            subTotal += item.total
-            cartItems += item.count
-        })
-        subTotal = parseFloat(subTotal.toFixed(2))
-        let tax = subTotal * 0.2
-        tax = parseFloat(tax.toFixed(2))
-        let total = subTotal + tax
-        total = parseFloat(total.toFixed(2))
+  // handle sidebar
+  handleSidebar = () => {
+    this.setState({ sidebarOpen: !this.state.sidebarOpen });
+  };
+  // hanldle sart
+  handleCart = () => {
+    this.setState({ cartOpen: !this.state.cartOpen });
+  };
+  //close cart
+  closeCart = () => {
+    this.setState({ cartOpen: false });
+  };
+  // open
+  openCart = () => {
+    this.setState({ cartOpen: true });
+  };
+  //  cart functionality
+  // increment
+  increment = id => {
+    let tempCart = [...this.state.cart];
+    const cartItem = tempCart.find(item => item.id === id);
+    cartItem.count++;
+    cartItem.total = cartItem.count * cartItem.price;
+    cartItem.total = parseFloat(cartItem.total.toFixed(2));
+    this.setState(
+      () => {
         return {
-            cartItems,
-            subTotal,
-            tax,
-            total
-        }
-    }
+          cart: [...tempCart]
+        };
+      },
+      () => {
+        this.addTotals();
+        this.syncStorage();
+      }
+    );
+  };
 
-    //add totals
-    addTotals = () => {
-        const totals = this.getTotals()
-        this.setState({
-            cartItem: totals.cartItems,
-            cartSubTotal: totals.subTotal,
-            cartTax: totals.tax,
-            cartTotal: totals.total
-        })
+  // decrement
+  decrement = id => {
+    let tempCart = [...this.state.cart];
+    const cartItem = tempCart.find(item => item.id === id);
+    cartItem.count = cartItem.count - 1
+    if(cartItem.count === 0) {
+        this.removeItem(id)
+    } else {
+        cartItem.total = cartItem.count * cartItem.price;
+        cartItem.total = parseFloat(cartItem.total.toFixed(2));
+        this.setState(
+            () => {
+              return {
+                cart: [...tempCart]
+              };
+            },
+            () => {
+              this.addTotals();
+              this.syncStorage();
+            }
+          );
     }
+  };
 
-    //sync storage
-    syncStorage = () => {
-        localStorage.setItem("cart", JSON.stringify(this.state.cart))
-    }
+  // removeItem
+  removeItem = id => {
+    let tempCart = [...this.state.cart];
+    tempCart = tempCart.filter(item => item.id !== id);
+    this.setState(
+      {
+        cart: [...tempCart]
+      },
+      () => {
+        this.addTotals();
+        this.syncStorage();
+      }
+    );
+  };
 
-    //add to cart
-    addToCart = (id) => {
-        let tempCart = [...this.state.cart]
-        let tempProducts = [...this.state.storeProducts]
-        let tempItem = tempCart.find(item => item.id === id)
-        if (!tempItem) {
-            tempItem = tempProducts.find(item => item.id === id)
-            let total = tempItem.price
-            let cartItem = { ...tempItem, count: 1, total }
-            tempCart = [...tempCart, cartItem]
-        } else {
-            tempItem.count++
-            tempItem.total = tempItem.price * tempItem.count
-            tempItem.total = parseFloat(tempItem.total.toFixed(2))
-        }
-        this.setState(() => {
-            return { cart: tempCart }
-        }, () => {
-            this.addTotals()
-            this.syncStorage()
-            this.openCart()
-        })
-    }
-    //set single product
-    setSingleProduct = (id) => {
-        let product = this.state.storeProducts.find(item => item.id === id)
-        localStorage.setItem("singleProduct", JSON.stringify(product))
-        this.setState({
-            singleProduct: { ...product },
-            loading: false
-        })
-    }
-    //handle side bar
-    handleSidebar = () => {
-        this.setState({ sidebarOpen: !this.state.sidebarOpen })
-    }
-    //handle cart
-    handleCart = () => {
-        this.setState({ cartOpen: !this.state.cartOpen })
+  //clear cart
+  clearCart = () => {
+    this.setState({
+        cart:[]
+    },  () => {
+        this.addTotals();
+        this.syncStorage();
+      }
+    )
+  };
 
-    }
-
-    //open cart
-    closeCart = () => {
-        this.setState({ cartOpen: false })
-    }
-
-    //close cart
-    openCart = () => {
-        this.setState({ cartOpen: true })
-    }
-
-    //cart functionality
-    //increment
-    increment = (id) => {
-        console.log(id)
-    }
-    //decrement
-    decrement = (id) => {
-        console.log(id)
-    }
-    //remove
-    removeItem = (id) => {
-        console.log(id)
-    }
-    //clear
-    clearCart = () => {
-        console.log("awesome you just cleared the cart")
-    }
-
-    render() {
-        return (
-            <ProductContext.Provider value={{
-                ...this.state,
-                handleSidebar: this.handleSidebar,
-                handleCart: this.handleCart,
-                closeCart: this.closeCart,
-                openCart: this.openCart,
-                addToCart: this.addToCart,
-                setSingleProduct: this.setSingleProduct,
-                increment: this.increment,
-                decrement: this.decrement,
-                removeItem: this.removeItem,
-                clearCart: this.clearCart
-            }}>
-                {this.props.children}
-            </ProductContext.Provider >
-        )
-    }
+  render() {
+    return (
+      <ProductContext.Provider
+        value={{
+          ...this.state,
+          handleSidebar: this.handleSidebar,
+          handleCart: this.handleCart,
+          closeCart: this.closeCart,
+          openCart: this.openCart,
+          addToCart: this.addToCart,
+          setSingleProduct: this.setSingleProduct,
+          increment: this.increment,
+          decrement: this.decrement,
+          removeItem: this.removeItem,
+          clearCart: this.clearCart
+        }}
+      >
+        {this.props.children}
+      </ProductContext.Provider>
+    );
+  }
 }
 
 const ProductConsumer = ProductContext.Consumer;
